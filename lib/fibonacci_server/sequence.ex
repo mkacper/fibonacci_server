7f8146f50865302f9204f6b358db3b2a6e970f04
Kacper Mentel <mentel.kk@gmail.com>
2024-07-14 14:24:10 +0200

    Improve code style

--- lib/fibonacci_server/sequence.ex
+++ lib/fibonacci_server/sequence.ex
@@ -1,15 +1,16 @@
 defmodule FibonacciServer.Sequence do
   def number(0), do: 0
   def number(1), do: 1
+  def number(n), do: number(n, _current_n = 1, {0, 1})
 
-  def number(n), do: number(n, _current_n = 0, {0, 1})
-  defp number(n, n, {n0, _n1}), do: n0
+  defp number(n, n, {_n0, n1}), do: n1
 
   defp number(n, current_n, {n0, n1}) do
     number(n, current_n + 1, {n1, n0 + n1})
   end
 
   def numbers(n), do: numbers(n - 1, [1, 0])
+
   defp numbers(0, numbers), do: Enum.reverse(numbers)
 
   defp numbers(n, [n0 | [n1 | _]] = numbers) do
