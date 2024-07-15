e2430d451c7b20c8687eec158c7e4b59b8937e3e
Kacper Mentel <mentel.kk@gmail.com>
2024-07-15 13:38:13 +0200

    WIP: Support basic pagination

--- lib/fibonacci_server_web/controllers/fibonacci_controller.ex
+++ lib/fibonacci_server_web/controllers/fibonacci_controller.ex
@@ -2,13 +2,35 @@ defmodule FibonacciServerWeb.FibonacciController do
   alias FibonacciServer.Fibonacci
   use FibonacciServerWeb, :controller
 
-  def sequence(conn, %{"number" => number} = params) do
+  @default_page_size 100
+
+  def sequence(conn, %{"number" => _} = params) do
     params = validate_params(params)
-    sequence = Fibonacci.sequence(params["number"])
+    sequence = Fibonacci.sequence(number(params), start_from(params))
     json(conn, sequence)
   end
 
-  defp validate_params(%{"number" => number}) do
-    %{"number" => String.to_integer(number)}
+  defp validate_params(%{"number" => number} = params) do
+    %{
+      "number" => String.to_integer(number),
+      "page_size" =>
+        (params["page_size"] && String.to_integer(params["page_size"])) || @default_page_size,
+      "page_number" => (params["page_number"] && String.to_integer(params["page_number"])) || 1
+    }
   end
+
+  defp number(%{"page_size" => p_size, "page_number" => p_number, "number" => number}) do
+    if number > p_size do
+      current_number = p_size * p_number - 1
+      (current_number < number && current_number) || number
+    else
+      number
+    end
+  end
+
+  defp start_from(%{"page_size" => size, "page_number" => number}) do
+    number * size - size
+  end
+
+  defp start_from(_), do: 0
 end
--- test/fibonacci_server_web/controllers/fibonacci_controller_test.exs
+++ test/fibonacci_server_web/controllers/fibonacci_controller_test.exs
@@ -15,6 +15,48 @@ defmodule FibonacciServerWeb.FibonacciControllerTest do
       # then
       assert json_response(conn, 200) == expected_seq
     end
+
+    test "returns 100 numbers at most by default", %{
+      conn: conn
+    } do
+      # given
+      number = 112
+
+      # when
+      conn = get(conn, ~p"/api/sequence?number=#{number}")
+
+      # then
+      assert resp = json_response(conn, 200)
+      assert Enum.map(0..99, & &1) == Enum.map(resp, & &1["index"])
+    end
+
+    test "respects `page_number` and `page_size` query params", %{
+      conn: conn
+    } do
+      # given
+      number = 19
+      page_size = 7
+      expected_1st_page = Enum.slice(fibonacci_sequence_resp(number), 0..6)
+      expected_2nd_page = Enum.slice(fibonacci_sequence_resp(number), 7..13)
+      expected_3rd_page = Enum.slice(fibonacci_sequence_resp(number), 14..19)
+
+      # when
+      for {page_number, expected_result} <- [
+            {1, expected_1st_page},
+            {2, expected_2nd_page},
+            {3, expected_3rd_page}
+          ] do
+        conn =
+          get(
+            conn,
+            ~p"/api/sequence?number=#{number}&page_size=#{page_size}&page_number=#{page_number}"
+          )
+
+        # then
+        assert resp = json_response(conn, 200)
+        assert expected_result == resp
+      end
+    end
   end
 
   # Helpers
